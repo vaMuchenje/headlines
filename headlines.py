b917da72eb66e184cdc7488f7794d4f03f2ed7f5
Tendi Muchenje <tendi@outlook.com>
2018-01-21 13:09:07 -0500

    Added Jinja template

--- headlines.py
+++ headlines.py
@@ -1,33 +1,24 @@
 import feedparser
 
-from flask import Flask
+from flask import Flask, render_template
 
 RSS_FEEDS = {
-'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
-'cnn':'http://rss.cnn.com/rss/edition.rss',
-'fox':'http://feeds.foxnews.com/foxnews/latest',
-'iol':'http://www.iol.co.za/cmlink/1.640'}
+'herald':'http://www.herald.co.zw/feed/',
+'newzimbabwe':'http://newzimbabwe.com/rss/rss.xml',
+'mafaro':'http://www.mafaro.co.uk/feeds/posts/default',
+'nehanda':'http://nehandaradio.com/feed/',
+'independent':'https://www.theindependent.co.zw/feed/',
+'newsday':'https://www.newsday.co.zw/feed/'}
 
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/<publication>')
-def get_news(publication="bbc"):
+def get_news(publication="herald"):
 	feed = feedparser.parse(RSS_FEEDS[publication])
-	first_article = feed['entries'][1]
-
-	return ''' 
-	<html>
-	<body>
-		<h1> Headlines </h1>
-		<b>{0}</b> <br/>
-		<i>{1}</i> <br/>
-		<p>{2}</p> <br/>
-	</body>
-<html>
-	'''.format(first_article.get("title"), first_article.get("published"), first_article.get('summary')
-		)
+	articles = feed['entries']
+	return render_template("home.html", articles=articles)
 
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
